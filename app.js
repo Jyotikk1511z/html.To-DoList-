12ca08ae212bfa8e573e9dd3aa8583893a9fde2b
Jyotikk1511z <152763637+Jyotikk1511z@users.noreply.github.com>
2024-02-26 23:30:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// list - to show all todos
+// add - to add a todo
+// delete - to delete a task
+// quit - to exit the todo
+
+let todo = [];
+
+let req = prompt("Please enter your request");
+
+while(true) {
+    if(req == "quit") {
+        console.log("Quitting app");
+        break;
+    }
+
+    if(req == "list") {
+        console.log("-------------");
+        for(let i=0; i<todo.length; i++) {
+
+             
+            // for(task of todo) {
+                console.log(i , todo[i]);
+            }
+            console.log("-------------");
+        } else if(req == "add") {
+             let task = prompt("Please enter the task you want to add");  
+             todo.push(task);
+             console.log("task added !");
+        }
+        else if(req == "delete") {
+           let idx = prompt("Please enter the task index");
+           todo.splice(idx , 1);
+           console.log("Task Deleted !");
+
+        }
+        else { 
+            console.log("Wrong Request !");
+        }
+        req = prompt("Please enter your request");
+    }
+
